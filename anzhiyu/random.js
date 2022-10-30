b45bd0aebcd6068103a70669b688943f041ac6dc
charles-webber <1484145749@qq.com>
2022-10-31 01:30:13 +0800

    Site updated: 2022-10-31 01:30:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/10/29/hello-world/","2022/10/29/answer/","2022/10/29/good/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/10/29/answer/","2022/10/29/hello-world/","2022/10/29/good/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
