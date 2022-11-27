b9d40234825135fb7d05bb0fc0535e3b190ada0e
charles-webber <1484145749@qq.com>
2022-11-27 18:33:49 +0800

    Site updated: 2022-11-27 18:33:49

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2022/10/29/answer/","2022/11/26/English assignment restaurant interview role play/","2022/10/29/hello-world/","2022/11/27/预习作业：/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/11/26/English assignment restaurant interview role play/","2022/10/29/answer/","2022/10/29/hello-world/","2022/11/27/预习作业：/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
